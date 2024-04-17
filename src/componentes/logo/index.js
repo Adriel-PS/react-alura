b89c22d3d201579f250dfee79caaaa391f455829
adriel_pesi <texte.0072002@gmail.com>
2024-04-17 17:27:28 -0300

    aula 4

--- src/componentes/logo/index.js
+++ src/componentes/logo/index.js
@@ -1,12 +1,22 @@
 import logo from '../../imagens/logo.svg'
-import './estilo.css'
+import styled from 'styled-components'
+
+const LogoContainer = styled.div `
+  display: flex;
+  font-size: 30px;
+  margin-left: 1%;
+`
+
+const LogoImage = styled.img`
+  margin-right:10px ;
+`
 
 function Logo(){
     return(
-        <div className='logo'>
-          <img src={logo} alt='logo da alura book ' className='logo-img'></img>
+        <LogoContainer>
+          <LogoImage src={logo} alt='logo da alura book '/>
           <p><strong>alura</strong> books</p>
-        </div>
+        </LogoContainer>
     )
 }
 
--- src/componentes/opcoesHeader/index.js
+++ src/componentes/opcoesHeader/index.js
@@ -1,14 +1,29 @@
-import './estilo.css'
+import styled from 'styled-components'
 
 const textoOpcoes= ['CATEGORIA','MINHA ESTANTE','FAVORITO']
 
+const Opcao = styled.li `
+    font-size: 16px ;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    height: 100%;
+    padding: 0 5px;
+    cursor: pointer;
+    min-width: 120px;
+`
+const Opicoes = styled.ul `
+    display: flex;
+`
+
 function OpcoesHeader(){
     return(
-        <ul className='opcoes'> 
+        <Opicoes> 
           { textoOpcoes.map( (texto) => (
-              <li className='opcao'><p> {texto}</p></li>
+              <Opcao><p> {texto}</p></Opcao>
             ) ) }
-        </ul>
+        </Opicoes>
     )
 }
 
